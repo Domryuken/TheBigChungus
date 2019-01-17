b78ec40e1d2f65a226911aeae54562b54b3fddc1
Domryuken <ladonjackson@hotmail.co.uk>
2019-01-17 16:21:50 +0000

    split into 3 parts

--- app/src/main/java/com/chungus/big/the/thebigchungus/MainActivity.java
+++ app/src/main/java/com/chungus/big/the/thebigchungus/MainActivity.java
@@ -1,13 +1,60 @@
 package com.chungus.big.the.thebigchungus;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem menuItem) {
+        // set item as selected to persist highlight
+        menuItem.setChecked(true);
+        // close drawer when item is tapped
+        mDrawerLayout.closeDrawers();
+
+        int selection = menuItem.getOrder();
+
+        if(selection == 0){
+            replaceFragment(new WorkoutFragment());
+        } else if(selection == 1){
+            replaceFragment(new FoodFragment());
+        } else if(selection == 2){
+            replaceFragment(new HabitsFragment());
+        }
+
+        return true;
+    }
+
+    public void replaceFragment(Fragment destFragment){
+        // First get FragmentManager object.
+        FragmentManager fragmentManager = this.getSupportFragmentManager();
+
+        // Begin Fragment transaction.
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        // Replace the layout holder with the required Fragment object.
+        fragmentTransaction.replace(R.id.content_frame, destFragment);
+
+        // Commit the Fragment replace action.
+        fragmentTransaction.commit();
     }
 }
---
+++ app/src/main/java/com/chungus/big/the/thebigchungus/WorkoutFragment.java
@@ -0,0 +1,18 @@
+package com.chungus.big.the.thebigchungus;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WorkoutFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View retView = inflater.inflate(R.layout.workout_fragment, container, false);
+        return retView;
+    }
+}
